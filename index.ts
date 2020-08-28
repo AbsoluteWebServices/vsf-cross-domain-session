f340ebf0cea19a25b3b2d1eb556daade3ea6a689
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2020-08-28 17:14:45 +0300

    Initial commit

---
+++ index.ts
@@ -0,0 +1,6 @@
+import { StorefrontModule } from '@vue-storefront/core/lib/modules'
+
+export const KEY = 'cross-domain-session'
+
+export const CrossDomainSessionModule: StorefrontModule = function ({ store, router, appConfig }) {
+}
